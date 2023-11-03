24c6752a761343f280723016ca3f03fb619f58ed
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-11-04 00:11:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+const loginForm = document.getElementById('loginForm');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const rememberCheckbox = document.getElementById('checkbox');
+    const submitButton = document.getElementById('submit');
+    const existingButton = document.createElement('button');
+    existingButton.textContent = 'Login as existing user';
+    existingButton.id = 'existing';
+    existingButton.style.display = 'none';
+
+    // Check if saved user details exist in localStorage
+    if (localStorage.getItem('savedUsername') && localStorage.getItem('savedPassword')) {
+      existingButton.style.display = 'block';
+    }
+
+    document.body.appendChild(existingButton);
+
+    // Event listener for the form submission
+    loginForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const username = usernameInput.value;
+      const password = passwordInput.value;
+
+      if (rememberCheckbox.checked) {
+        localStorage.setItem('savedUsername', username);
+        localStorage.setItem('savedPassword', password);
+      } else {
+        localStorage.removeItem('savedUsername');
+        localStorage.removeItem('savedPassword');
+      }
+
+      alert(`Logged in as ${username}`);
+
+      // If there are saved details, show existing button
+      if (localStorage.getItem('savedUsername') && localStorage.getItem('savedPassword')) {
+        existingButton.style.display = 'block';
+      }
+    });
+
+    // Event listener for the "Login as existing user" button
+    existingButton.addEventListener('click', function() {
+      const savedUsername = localStorage.getItem('savedUsername');
+      alert(`Logged in as ${savedUsername}`);
+    });
